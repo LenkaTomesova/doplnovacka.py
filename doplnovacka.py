553e61c84de057e39a9e9bab272ef89e02f40a5d
Lenka Tomesova <letomesova@gmail.com>
2019-05-22 14:16:31 +0200

    optimalizace kódu

--- doplnovacka.py
+++ doplnovacka.py
@@ -20,22 +20,34 @@ else: vysledek.append(prvekseznamu)
 
 původní slovo přidat do seznamu
 4 seznam spojit pomocí mezery do řetězce, řetězec uložit do nového souboru
+
+Ukol:
+Proveďte optimalizaci kódu programu "doplnovacka.py" tak,
+abys problém řešila s využitím vlastní funkce (vstupními parametry bude vložený text
+či název souboru, který se má načíst, a celé číslo představující, kolikáté slovo textu
+se má zaměnit za hvězdičky). Tuto optimalizovanou verzi nahraj do existujícího projektu
+na GitHubu. 
 """
-with open("lorem.txt") as f:
-	text = f.read()
-f.closed
+import os.path
+
+vstup = input("Vložte text nebo název souboru, ve kterém chcete vynechat slova: ")
+vynechaneSlovo = int(input("Zadejte, kolikáté slovo chcete vynechat: "))
+#with open("lorem.txt") as f:
+#	text = f.read()
+#f.closed
 
 #print(text)
-text = text.replace("\n", "# ")
-seznam = []
-seznam = text.split(" ")
-vysledek = []
-interpunkce = ".,?!-;\""
+def uprava Textu(text):
+	text = text.replace("\n", "# ")
+	seznam = []
+	seznam = text.split(" ")
+	vysledek = []
+	interpunkce = ".,?!-;\""
 # print(seznam)
-for i in range(0, len(seznam)):
-	slovo = seznam[i]
-	slovoUpraveno = ""
-	if((i + 1) % 5 == 0):
+	for i in range(0, len(seznam)):
+		slovo = seznam[i]
+		slovoUpraveno = ""
+		if((i + 1) % vynechaneSlovo == 0):
 		for pismeno in slovo:
 			if pismeno not in interpunkce:
 				slovoUpraveno += "*"
@@ -44,6 +56,15 @@ for i in range(0, len(seznam)):
 		vysledek.append(slovoUpraveno)
 	else:
 		vysledek.append(slovo)
+
 s=" ".join(vysledek)
 s=s.replace("#", "\n")
 print(s)
+
+jesoubor = os.path.isfile(vstup)
+if jeSoubor:
+	with open(vstup) as f:
+		vstup = f.read()
+		upravaTextu(vstup)
+else:
+	upravaTextu(vstup)
